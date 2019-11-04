85a97aeb2f57418ba43f3e1d32809e88fe0a159e
Hushan Khan <hushain1990@gmail.com>
2019-11-04 15:47:32 +0530

    Initial Commit

---
+++ WebHelperLib/WebHelperLib.h
@@ -0,0 +1,19 @@
+//
+//  WebHelperLib.h
+//  WebHelperLib
+//
+//  Created by Hushan on 11/4/19.
+//  Copyright © 2019 Hushan M Khan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WebHelperLib.
+FOUNDATION_EXPORT double WebHelperLibVersionNumber;
+
+//! Project version string for WebHelperLib.
+FOUNDATION_EXPORT const unsigned char WebHelperLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebHelperLib/PublicHeader.h>
+
+
